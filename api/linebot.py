95e73f5278794f3a6654d78a0ac2712aba191260
EthanTsai69 <ethan.tsai.31018@aaia.ntpc.edu.tw>
2025-04-18 18:10:08 +0800

    Update linebot.py

--- api/linebot.py
+++ api/linebot.py
@@ -33,7 +33,7 @@ def callback():
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.text == 'test':
-        reply_message = "第36行的reply_message=改成自己想傳送的訊息"
+        reply_message = "Hello"
     else:
         reply_message = event.message.text
     line_bot_api.reply_message(
