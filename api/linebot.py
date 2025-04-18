3137722f990c45ac8c5ce603bab0526771d70853
EthanTsai69 <ethan.tsai.31018@aaia.ntpc.edu.tw>
2025-04-18 18:30:49 +0800

    uwbahfjakf

--- api/linebot.py
+++ api/linebot.py
@@ -33,7 +33,7 @@ def callback():
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.text == 'test':
-        reply_message = "Im blind Im deaf I wanna be a ref"
+        reply_message = "Guys, Im Gay"
     else:
         reply_message = event.message.text
     line_bot_api.reply_message(
